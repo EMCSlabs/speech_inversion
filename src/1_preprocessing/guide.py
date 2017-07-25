5e6e2c736a1823cfa86faa9ed80d5f16fb4c1d23
jaekookang <jkang@gradcenter.cuny.edu>
2017-07-25 16:34:04 +0900

    upload guide.py

---
+++ src/1_preprocessing/guide.py
@@ -0,0 +1,27 @@
+# Guide for data processing
+import numpy as np
+from tools import read_data
+import scipy.io as sio
+
+# Load data
+acous_train = '../../data/vowels/train/acoustics_train.mat'
+artic_train = '../../data/vowels/train/articulation_train.mat'
+
+acous_raw, artic_raw, labels = read_data.load_data(acous_train, artic_train)
+# acous_raw and artic_raw is list variable
+# each element in the list is a numpy array
+acous_dim = acous_raw[0].shape[0]
+artic_dim = artic_raw[0].shape[0]
+
+print('Acoustics data: {:d}'.format(len(acous_raw)))
+print('Acoustics dimension: {:d}\n'.format(acous_dim))
+print('Articulation data: {:d}'.format(len(artic_raw)))
+print('Articulation dimension: {:d}\n'.format(artic_dim))
+
+# Zero padding
+# (batch size x max length x features)
+acous_padded = read_data.zero_padding(acous_raw)
+artic_padded = read_data.zero_padding(artic_raw)
+print('padded acoustics:', acous_padded.shape)
+print('padded articulation:', artic_padded.shape)
+
