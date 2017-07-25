1fb51f9063a5398536f1646231562ef5ff6aab5b
jaekookang <jkang@gradcenter.cuny.edu>
2017-07-25 15:53:19 +0900

    update data

--- src/1_preprocessing/tools/normalize.py
+++ src/1_preprocessing/tools/normalize.py
@@ -1,3 +1,6 @@
+import numpy as np
+
+
 def zscore(data, axis=1):
     '''
     인풋-아웃풋 데이터를 z-score normalize하기 위한 함수
@@ -6,13 +9,15 @@ def zscore(data, axis=1):
     mu = np.mean(data, axis, keepdims=True)
     std = np.std(data, axis, keepdims=True)
     out = (data - mu) / std
-    return out, mu, std 
+    return out, mu, std
+
 
 def zscore_reverse(data, mu, std):
     '''
     z-score로부터 원래 데이터로 복구하는 함수
     '''
-    return data*std + mu
+    return data * std + mu
+
 
 def minmax(data, minmax_range=(0.1, 0.9), axis=1):
     '''
@@ -22,8 +27,10 @@ def minmax(data, minmax_range=(0.1, 0.9), axis=1):
     min_val = np.min(data, axis=1, keepdims=True)
     max_val = np.max(data, axis=1, keepdims=True)
     min_new, max_new = minmax_range
-    out = (data - min_val)/(max_val - min_val)*(max_new - min_new) + min_new
+    out = (data - min_val) / (max_val - min_val) * \
+        (max_new - min_new) + min_new
     return out, (min_val, max_val, min_new, max_new)
+
 
 def minmax_reverse(data, param):
     '''
@@ -32,11 +39,13 @@ def minmax_reverse(data, param):
     min_val, max_val, min_new, max_new = param
     range_old = max_val - min_val
     range_new = max_new - min_new
-    out = (data - min_new)*range_old/range_new + min_val
+    out = (data - min_new) * range_old / range_new + min_val
     return out
+
 
 def sigmoid(data):
     return 1 / (1 + np.exp(-data))
+
 
 def sigmoid_reverse(data):
     return np.log(data)
--- src/1_preprocessing/tools/read_data.py
+++ src/1_preprocessing/tools/read_data.py
@@ -1,19 +1,45 @@
-# Read articulation.pckl and acoustics.pckl
 import matplotlib.pyplot as plt
-from matplotlib import animation, rc
 from IPython.display import HTML
+from matplotlib import animation
 import numpy as np
+import tensorflow as tf
 import scipy.io as sio
-import pickle
-import random
-import csv
+
 
 def load_data(acous_dir, artic_dir):
-    acous_raw = sio.loadmat(acous_dir)['acoustics'][0,0]
-    artic_raw = sio.loadmat(artic_dir)['articulation'][0,0]
+    acous_raw = sio.loadmat(acous_dir)['acoustics'][0, 0]
+    artic_raw = sio.loadmat(artic_dir)['articulation'][0, 0]
+    labels = list(acous_raw.dtype.fields.keys())
+
     acous_raw = acous_raw.tolist()
     artic_raw = artic_raw.tolist()
-    return acous_raw, artic_raw
+    return acous_raw, artic_raw, labels
+
+
+def zero_padding(data):
+    # data: list of (num_feature x num_seq)
+    # padded: (num_example x num_seq x num_feature)
+    length = [data[i].shape[1] for i in range(len(data))]
+    max_length = max(length)
+    padded = np.zeros((len(data), max_length, data[0].shape[0]))
+    for i in range(len(data)):
+        padded[i, :data[i].shape[1], :data[0].shape[0]] = np.transpose(data[i])
+    return padded
+
+
+def length(data):
+    used = tf.sign(tf.reduce_max(tf.abs(data), reduction_indices=2))
+    length = tf.reduce_sum(used, reduction_indices=1)
+    length = tf.cast(length, tf.int32)
+    return length
+
+
+def divide_timestep(data, time_step, max_time):
+    out = []
+    for i in range(max_time - time_step + 1):
+        out.append(data[:, i:i + time_step, :])
+    return out
+
 
 def check_original_plot(x, y):
     '''
@@ -26,21 +52,21 @@ def check_original_plot(x, y):
     img = ax1.imshow(np.flipud(x), aspect='auto')
     ax1.set_ylabel('MFCC')
     pp, = ax1.plot([], [], 'k-')
-    
+
     ax2 = fig.add_subplot(212)
     ax2.set_xlim((-60, 20))
     ax2.set_ylim((-30, 30))
     dots, = ax2.plot([], [], 'bo')
     ax2.set_ylabel('Articulation')
-    
+
     def plot_frames(n_frame):
-        dots.set_data(y[::2, n_frame], 
+        dots.set_data(y[::2, n_frame],
                       y[1::2, n_frame])
         pp.set_data(n_frame, [0, x.shape[0]])
         return ([dots, pp],)
 
-    anim = animation.FuncAnimation(fig, plot_frames, 
-                                   frames=x.shape[1], 
-                                   interval=50, blit=False, 
+    anim = animation.FuncAnimation(fig, plot_frames,
+                                   frames=x.shape[1],
+                                   interval=50, blit=False,
                                    repeat=False)
     return HTML(anim.to_html5_video())
